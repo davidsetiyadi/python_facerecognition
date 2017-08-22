30736f7ab3f7d593e2b948c73c8714dffa02de29
david <davidsetiadi11@gmail.com>
2017-08-22 16:51:46 +0700

    test

---
+++ Sample/test.py
@@ -0,0 +1,17 @@
+import base64
+
+def convert(image):
+    f = open(image)
+    data = f.read()
+    f.close()
+
+    string = base64.b64encode(data)
+    print (string,'david')
+    convert = base64.b64decode(string)
+
+    t = open("example1.png", "w+")
+    t.write(convert)
+    t.close()
+
+if __name__ == "__main__":
+    convert("imgpsh_fullsize.png")
